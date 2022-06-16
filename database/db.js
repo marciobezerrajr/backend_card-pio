a6ce7888d33057559234721a1926e8640e25ae9a
Márcio <marcioalvesbj@gmail.com>
2022-06-16 14:00:11 -0300

    Ajust url and dotent variables

--- database/db.js
+++ database/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
 
-const result = dotenv.config()
-
+require('dotenv').config()
 require("../models/Category")
 require("../models/Snack")
 require("../models/User")
 
-mongoose.connect(`mongodb+srv://${result.parsed.MONGO_USER}:${result.parsed.MONGO_PASS}@cluster0.fawuu.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
